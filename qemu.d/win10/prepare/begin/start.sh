eaf71903d72d91b073bc9169f19ba5c91ae8d454
Gabriel Morrison Lima Dantas <gabrielmldantas@gmail.com>
2021-05-24 10:55:45 -0300

    Initial commit

---
+++ qemu.d/win10/prepare/begin/start.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Helpful to read output when debugging
+set -x
+
+# load variables we defined
+source /etc/libvirt/hooks/kvm.conf
+
+# Terminate sessions safely
+for s in $(loginctl --no-legend list-sessions | cut -d' ' -f1)
+do
+	d="$(loginctl show-session "$s" --property=Desktop --value)"
+	[[ -n "$d" ]] && loginctl terminate-session "$s"
+done
+
+# Stop display manager
+systemctl stop display-manager.service
+
+# Stop all pulseaudio instances
+killall pulseaudio
+
+# Unbind VTconsoles
+echo 0 | tee /sys/class/vtconsole/vtcon*/bind >/dev/null
+
+# Unload the modules
+# Retry until unload succeeds
+for m in "${MODULES[@]}"
+do
+	ret=0
+	for _ in $(seq 4)
+	do
+		ret=0
+		modprobe --remove --force "$m" || ret=1
+		lsmod | grep -q -- '\b'"$m"'\b' && ret=1
+		[[ $ret -eq 0 ]] && break
+		sleep 2
+	done
+	[[ $ret -ne 0 ]] && exit 1
+done
+
+# Detach the pci devices
+for pci_dev in "${VIRSH_PCI_DEVS[@]}"
+do
+	virsh nodedev-detach "$pci_dev"
+done
+
+# load vfio
+modprobe vfio
+modprobe vfio_pci
+modprobe vfio_iommu_type1
+
+# Switch to performance governor
+cpupower frequency-set -g performance
+
---
+++ qemu.d/win10/release/end/revert.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Debug
+set -x
+
+# Load variables
+source /etc/libvirt/hooks/kvm.conf
+
+# unload vfio-pci
+modprobe --remove --force vfio_pci
+modprobe --remove --force vfio_iommu_type1
+modprobe --remove --force vfio
+
+# Rebind PCI devs
+for pci_dev in "${VIRSH_PCI_DEVS[@]}"
+do
+	virsh nodedev-reattach "$pci_dev"
+done
+
+# rebind VTconsoles
+echo 1 | tee /sys/class/vtconsole/vtcon*/bind >/dev/null
+
+# Reload modules
+for m in "${MODULES[@]}"
+do
+	modprobe "$m"
+done
+
+# switch back to schedutil governor
+cpupower frequency-set -g schedutil
+
+# Restart Display service
+systemctl start display-manager.service
+
